73c0367cf832059a0c50d04f3e8f63b879946562
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-10 11:26:47 +0100

    Accept arguments from the command line

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("wifi=123");
+console.log("PW-Manager");
+
+const args = process.argv.slice(2);
+const passwordname = args[0];
+console.log(`You want to know the password of ${passwordname}`);
+
+if (passwordname === "wifi") {
+  console.log("Password is 123");
+} else {
+  console.log("Unknown password");
+}
